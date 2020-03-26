26b4ac0c947bfddd81c0df0a70a8cff3ec73cc91
bashcal <bashar.atoum@mavs.uta.edu>
2020-03-26 18:12:11 -0500

    fixes

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='PyBoltzTestData',  # Required
-    packages=['TestData'],
+    packages=['PyBoltzTestData'],
     version='1.0.0',  # Required
     description='Simple project made to export data',  # Optional
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
     install_requires=['tables', 'numpy==1.16.1'],  # Optional
     package_data={  # Optional
-        'TestData': ['*.csv','*.npy','*.h5','Tests.npy'],
+        'PyBoltzTestData': ['*.csv','*.npy','*.h5','Tests.npy'],
     },
 )
