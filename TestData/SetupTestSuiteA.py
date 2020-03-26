44548ec8cb723f7610540baf0c63a335e2dbf967
bashcal <bashar.atoum@mavs.uta.edu>
2020-03-26 17:45:59 -0500

    added setup script for packaging

--- TestData/SetupTestSuiteA.py
+++ TestData/SetupTestSuiteA.py
@@ -1,15 +1,22 @@
 import numpy as np
 from tables import *
 from Definitions import *
-
+import os 
 # import magboltz outputs for ArCH4 ( 90% - 10%)
 MBAr = np.load("ArCH4.npy")
+
+P10B    = np.genfromtxt("P10-B.csv", delimiter=',')
+
 
 # import Data for ArCH4 ( 90% - 10%)
 ArDl = np.genfromtxt("ArCH4-Dl.csv", delimiter=',')
 ArDt = np.genfromtxt("ArCH4-Dt.csv", delimiter=',')
 ArV = np.genfromtxt("ArCH4-vel.csv", delimiter=',')
 
+# Ionisation rate data for 100% nitrogen Gas.
+Lima = np.loadtxt("Lima.csv", delimiter=',',skiprows=2)
+
+# Magboltz transverse diffusion values from magboltz
 magB    = np.array([0.9944e4, 0.8642e4 ,0.7509e3, 0.6307e4, 0.1629e4, 0.1218e4, 0.9223e3, 0.3616e4, 0.2429e4])
 magBer  = np.array([3.87    , 2.31     ,2.62    , 3.39    , 2.92    , 2.60    , 3.43    , 2.79    , 3.13   ])
 
@@ -33,18 +40,18 @@ TestsFile['T1/Input/GasIDs'] = [2,8,0,0,0,0]
 TestsFile['T1/Input/GasFractions'] = [90,10,0,0,0,0]
 TestsFile['T1/Input/TemperatureCentigrade'] = 23
 TestsFile['T1/Input/Pressure_Torr'] = 750
-TestsFile['T1/Input/EField'] = MBAr[80:81,4][::skip][0] # taken from data
+TestsFile['T1/Input/EField'] = MBAr[80:81,4][::4][0] # taken from data
 TestsFile['T1/Input/BField_Mag'] = 0.0 
 TestsFile['T1/Input/BField_Angle'] = 0.0 
 TestsFile['T1/Input/Console_Output_Flag'] = 0 
 TestsFile['T1/Input/Which_Angular_Model'] = 2
 # Magboltz data
-TestsFile['T1/Output/MBdt'] = MBAr[80:81,13][::skip][0]
-TestsFile['T1/Output/MBdtE'] = MBAr[80:81,13][::skip][0]*MBAr[80:81,14][::skip][0]
-TestsFile['T1/Output/MBdl'] = MBAr[80:81,11][::skip][0]
-TestsFile['T1/Output/MBdlE'] = MBAr[80:81,11][::skip][0]*MBAr[80:81,12][::skip][0]
-TestsFile['T1/Output/MBvel'] = MBAr[80:81,5][::skip][0]
-TestsFile['T1/Output/MBvelE'] = MBAr[80:81,5][::skip][0] * MBAr[80:81,6][::skip][0]
+TestsFile['T1/Output/MBdt'] = MBAr[80:81,13][::4][0]
+TestsFile['T1/Output/MBdtE'] = MBAr[80:81,13][::4][0]*MBAr[80:81,14][::4][0]
+TestsFile['T1/Output/MBdl'] = MBAr[80:81,11][::4][0]
+TestsFile['T1/Output/MBdlE'] = MBAr[80:81,11][::4][0]*MBAr[80:81,12][::4][0]
+TestsFile['T1/Output/MBvel'] = MBAr[80:81,5][::4][0]
+TestsFile['T1/Output/MBvelE'] = MBAr[80:81,5][::4][0] * MBAr[80:81,6][::4][0]
 # Argon data
 TestsFile['T1/Output/dt'] = ArDt[1,1]
 TestsFile['T1/Output/dtE'] = ArDt[1,1] * 0.3
@@ -76,4 +83,24 @@ TestsFile['T2/Output/MBdtrE'] = (0.9944e4/0.7509e3) * np.sqrt((magBer[0]/100)**2
 TestsFile['T2/Output/dtr'] = P10B[3,1]
 TestsFile['T2/Output/dtrE'] = 0.5 * P10B[3,1]
 
-np.save(os.path.join(os.path.dirname(os.path.realpath(__file__)),"Tests"), gd)
+# Third Test
+TestsFile['T3/Input/NumberOfGases'] = 1
+TestsFile['T3/Input/MaxNumberOfCollisions'] = 40000000.0
+TestsFile['T3/Input/Enable_Penning'] = 0
+TestsFile['T3/Input/Enable_Thermal_Motion'] = 1
+TestsFile['T3/Input/Max_Electron_Energy'] = 0.0
+TestsFile['T3/Input/GasIDs'] = [16,0,0,0,0,0]
+TestsFile['T3/Input/GasFractions'] = [100,0,0,0,0,0]
+TestsFile['T3/Input/TemperatureCentigrade'] = 23
+TestsFile['T3/Input/Pressure_Torr'] = 750
+TestsFile['T3/Input/EField'] = 40000
+TestsFile['T3/Input/BField_Mag'] = 0.0
+TestsFile['T3/Input/BField_Angle'] = 0.0
+TestsFile['T3/Input/Console_Output_Flag'] = 0 
+TestsFile['T3/Input/Which_Angular_Model'] = 2
+# Nitrogen Alpha data
+TestsFile['T3/Output/AlphaSST'] = Lima[6,1]
+TestsFile['T3/Output/AlphaSSTE'] = Lima[6,1]*0.5
+
+
+np.save(os.path.join(os.path.dirname(os.path.realpath(__file__)),"Tests"), TestsFile)
---
+++ setup.py
@@ -0,0 +1,19 @@
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='PyBoltzTestData',  # Required
+    packages=['TestData'],
+    version='1.0.0',  # Required
+    description='Simple project made to export data',  # Optional
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    install_requires=['tables', 'numpy==1.16.1'],  # Optional
+    package_data={  # Optional
+        'TestData': ['*.csv','*.npy','*.h5','Tests.npy'],
+    },
+)
